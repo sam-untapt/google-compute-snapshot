7a6459fd49866d938746adfe066f28878f7fb223
Sam Kenny <sam-untapt@users.noreply.github.com>
2016-10-06 22:45:10 -0400

    Update snapshot.sh
    changed to work for any instance - pass in the name

--- snapshot.sh
+++ snapshot.sh
@@ -1,71 +1,43 @@
 #!/usr/bin/env bash
 export PATH=$PATH:/usr/local/bin/:/usr/bin
 
-usage() {
-  echo -e "\nUsage: $0 [-d <days>]" 1>&2
-  echo -e "\nOptions:\n"
-  echo -e "    -d    Number of days to keep snapshots.  Snapshots older than this number deleted."
-  echo -e "          Default if not set: 7 [OPTIONAL]"
-  echo -e "\n"
-  exit 1
-}
-
-#
-# Get and set how long to keep snapshots. Default to 7 (days)
-#
-while getopts ":d:" o; do
-  case "${o}" in
-    d)
-      opt_d=${OPTARG}
-      ;;
-
-    *)
-      usage
-      ;;
-  esac
-done
-shift $((OPTIND-1))
-
-if [[ -n $opt_d ]];then
-  OLDER_THAN=$opt_d
-else
-  OLDER_THAN=7
+if [ $# -eq 0 ]
+  then
+    echo "Please pass the instance name"
+    return 
 fi
 
 #
 # CREATE DAILY SNAPSHOT
 #
-# get the device name for this vm
-DEVICE_NAME="$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/disks/0/device-name" -H "Metadata-Flavor: Google")"
 
-# get the device id for this vm
-DEVICE_ID="$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/id" -H "Metadata-Flavor: Google")"
+# set the device name
+DEVICE_NAME="$1"
 
-# get the zone that this vm is in
-INSTANCE_ZONE="$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google")"
+# get the zone  for this device
+INSTANCE_ZONE="$(gcloud compute instances list $DEVICE_NAME | grep $DEVICE_NAME | cut -d " " -f 3)"
 
-# strip out the zone from the full URI that google returns
-INSTANCE_ZONE="${INSTANCE_ZONE##*/}"
+# get the device id for this device
+DEVICE_ID="$(gcloud compute instances describe $DEVICE_NAME --zone ${INSTANCE_ZONE} | grep id: | sed s/id:// | sed s/"'"//g | sed -e 's/^[ \t]*//')"
 
 # create a datetime stamp for filename
 DATE_TIME="$(date "+%s")"
 
 # create the snapshot
-echo -e "[$(date -Iseconds)] \c"
-echo -e "$(gcloud compute disks snapshot ${DEVICE_NAME} --snapshot-names gcs-${DEVICE_NAME}-${DEVICE_ID}-${DATE_TIME} --zone ${INSTANCE_ZONE})"
+gcloud compute disks snapshot ${DEVICE_NAME} --no-user-output-enabled --snapshot-names gcs-${DEVICE_NAME}-${DEVICE_ID}-${DATE_TIME} --zone ${INSTANCE_ZONE}
 
 
 #
 # DELETE OLD SNAPSHOTS (OLDER THAN 7 DAYS)
 #
 
-# get a list of existing snapshots, that were created by this process (gcs-), for this vm disk (DEVICE_ID)
-SNAPSHOT_LIST="$(gcloud compute snapshots list --regexp "(.*gcs-.*)|(.*-${DEVICE_ID}-.*)" --uri)"
+# get a list of existing snapshots, that were created by this process (gcs-), for this vm disk (DEVICE_ID) and save to file for reference
+gcloud compute snapshots list --regexp "(.*gcs-${DEVICE_NAME}-.*)" --uri > snapshot_list_$DEVICE_NAME.txt
 
 # loop through the snapshots
-echo "${SNAPSHOT_LIST}" | while read line ; do
+cat snapshot_list_$DEVICE_NAME.txt | while read line ; do
 
-   # get the snapshot name from full URL that google returns
+# DELETE OLD SNAPSHOTS (OLDER THAN 7 DAYS)
    SNAPSHOT_NAME="${line##*/}"
 
    # get the date that the snapshot was created
@@ -75,12 +47,12 @@ echo "${SNAPSHOT_LIST}" | while read line ; do
    SNAPSHOT_DATETIME="$(date -d ${SNAPSHOT_DATETIME} +%Y%m%d)"
 
    # get the expiry date for snapshot deletion (currently 7 days)
-   SNAPSHOT_EXPIRY="$(date -d "-${OLDER_THAN} days" +"%Y%m%d")"
+   SNAPSHOT_EXPIRY="$(date -d "-7 days" +"%Y%m%d")"
 
    # check if the snapshot is older than expiry date
-    if [ $SNAPSHOT_EXPIRY -ge $SNAPSHOT_DATETIME ];then
-      # delete the snapshot
-      echo -e "[$(date -Iseconds)] \c"
-      echo "$(gcloud compute snapshots delete ${SNAPSHOT_NAME})"
-    fi
+if [ $SNAPSHOT_EXPIRY -ge $SNAPSHOT_DATETIME ];
+        then
+         # delete the snapshot
+         echo "$(gcloud compute snapshots delete ${SNAPSHOT_NAME} --quiet --no-user-output-enabled)"
+   fi
 done
